a0c14ffccfcb68719fe7078915684e2972c0e727
Viiic98 <xathovic@gmail.com>
2020-08-07 12:39:24 -0500

    Add readme file and header file

---
+++ 0x02-heap_insert/README.md
@@ -0,0 +1,9 @@
+# Heap Insert
+
+### Tasks
+
+#### [New node](./0-binary_tree_node.c)
+- Function that creates a binary tree node
+
+#### [Heap - Insert](./1-heap_insert.c)
+- Function that inserts a value into a Max Binary Heap
---
+++ 0x02-heap_insert/binary_trees.h
@@ -0,0 +1,35 @@
+#ifndef HEAP
+#define HEAP
+
+#include <stddef.h>
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s heap_t;
+
+struct node_v
+{
+    binary_tree_t *node;
+    int childs;
+    int deep;
+};
+typedef struct node_v ver_node;
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *tree);
+heap_t *heap_insert(heap_t **root, int value);
+
+#endif /* HEAP */
