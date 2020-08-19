3a2eb6fa2f0867918bfd0605dae55b3cf9119800
Viiic98 <xathovic@gmail.com>
2020-08-18 20:39:35 -0500

    Modify header file

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,9 +1,11 @@
-#ifndef SANDPILE_H
-#define SANDPILE_H
+#ifndef SANDPILES_H
+#define SANDPILES_H
 
 #include <stdio.h>
+#include <stdlib.h>
+
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 int verify_sandpile(int grid[3][3]);
 void sandpiles_convert(int grid[3][3]);
 
-#endif /* SANDPILE_H */
+#endif /* SANDPILES_H */
