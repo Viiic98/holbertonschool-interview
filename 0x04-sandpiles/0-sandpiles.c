4830f863faa5fca90a56145d2ac8c43d00c8d96e
Viiic98 <xathovic@gmail.com>
2020-08-18 20:01:40 -0500

    Add empty files

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,12 @@
+#include "sandpiles.h"
+/**
+ * sandpiles_sum - Sum two grids and then convert
+ * the result grid to sandpile
+ * @grid1: Grid number one
+ * @grid2: Grid number two
+ * Return: Nothing, just sum
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+
+}
---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,6 @@
+#ifndef SANDPILE_H
+#define SANDPILE_H
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
+#endif /* SANDPILE_H */
