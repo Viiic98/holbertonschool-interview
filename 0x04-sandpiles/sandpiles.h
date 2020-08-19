12e47410462804a5c5fbc66f030a92d6b92d8376
Victor Arteaga <xathovic@gmail.com>
2020-08-18 19:49:35 -0500

    Delete sandpiles.h

--- 0x04-sandpiles/sandpiles.h
+++
@@ -1,8 +0,0 @@
-#ifndef SANDPILE
-#define SANDPILE
-
-void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-int verify_sandpile(int grid[3][3]);
-void sandpiles_convert(int grid[3][3]);
-
-#endif /* SANDPILE */
