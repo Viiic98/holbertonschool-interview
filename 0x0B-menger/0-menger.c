c34531b8a23fe213cfcc6c0d84e7e757a36932de
Viiic98 <xathovic@gmail.com>
2020-10-07 14:18:55 -0500

    Remake the problem with a better solution

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -7,14 +7,26 @@
  */
 void menger(int level)
 {
-	int size = pow(3, level);
+	int size = pow(3, level), i = 0, j = 0, x = 0, y = 0;
+	char val = '#';
 
 	if (level < 0)
 		return;
-	if (size == 1)
-	{
-		printf("#\n");
-		return;
-	}
 
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			x = i, y = j, val = '#';
+			while (x > 0 || y > 0)
+			{
+				if (x % 3 == 1 && y % 3 == 1)
+					val = ' ';
+				x = x / 3;
+				y = y / 3;
+			}
+			printf("%c", val);
+		}
+		printf("\n");
+	}
 }
