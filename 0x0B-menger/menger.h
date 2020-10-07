094e60d9e20e7f5c19399e4ce91f55f8059af442
Viiic98 <xathovic@gmail.com>
2020-10-07 13:56:02 -0500

    Add the rest of file

--- 0x0B-menger/menger.h
+++ 0x0B-menger/menger.h
@@ -5,5 +5,8 @@
 #include <math.h>
 
 void menger(int level);
+void print_sponge(int size, char *sponge[size]);
+void add_one(int size, char *sponge[size], int x, int y, char val);
+void update_copy(int size, char *org[size], char *copy[size]);
 
 #endif /* MENGER_S */
