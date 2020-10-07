161bd588bdb44c002111a18a79edf54cd5b414e6
Viiic98 <xathovic@gmail.com>
2020-10-07 14:00:24 -0500

    Start of remake the code

--- 0x0B-menger/menger.h
+++ 0x0B-menger/menger.h
@@ -6,8 +6,6 @@
 #include <stdlib.h>
 
 void menger(int level);
-void print_sponge(int size, char *sponge[size]);
-void add_one(int size, char *sponge[size], int x, int y, char val);
-void update_copy(int size, char *org[size], char *copy[size]);
+
 
 #endif /* MENGER_S */
