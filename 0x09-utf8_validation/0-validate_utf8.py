2b7f3254c41ee0dc9b00f303f68c126419889de8
Viiic98 <xathovic@gmail.com>
2020-10-09 12:24:47 -0500

    Add utf-8 validation

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+def validUTF8(data):
+    """ determines if a given data set represents a valid UTF-8 encoding
+
+        Return: True if data is a valid UTF-8 encoding, else return False
+    """
+    n_bytes = 0
+    for x in data:
+        m = 1 << 7
+        if n_bytes == 0:
+            while x & m:
+                n_bytes += 1
+                m = m >> 1
+            if n_bytes:
+                n_bytes -= 1
+        else:
+            if x & m:
+                n_bytes -= 1
+            elif x & m >> 1:
+                return False
+    return n_bytes == 0
---
+++ 0x09-utf8_validation/README.md
@@ -0,0 +1,4 @@
+# UTF-8 Validation
+
+### [UTF-8 Validation](./0-validate_utf8.py)
+#### Method that determines if a given data set represents a valid UTF-8 encoding.
