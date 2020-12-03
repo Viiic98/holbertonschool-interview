02c7f436825e13c5accb3ab54a85d01c7883d0b5
Viiic98 <xathovic@gmail.com>
2020-12-02 20:27:36 -0500

    Improve solution

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -6,7 +6,17 @@ def rain(walls):
     """ calculate how much water will be retained after it rains """
     if type(walls) is not list or len(walls) == 0:
         return 0
+    wall = 0
+    a = 0
     n = 0
-    for i in range(1, len(walls) - 1):
-        n += walls[i]
+    fil = 0
+    for i in range(1, len(walls)):
+        if walls[i] > wall:
+            n += wall * a - fil
+            wall = walls[i]
+            a = 0
+            fil = 0
+        else:
+            fil += walls[i]
+            a += 1
     return n
