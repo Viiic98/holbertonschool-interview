0b5daeabcd6ab4627080c79416f7f07366f25b5f
Viiic98 <xathovic@gmail.com>
2021-02-17 21:22:26 -0500

    Add merge sort

---
+++ 0x18-merge_sort/0-merge_sort.c
@@ -0,0 +1,79 @@
+#include "sort.h"
+/**
+ * merge - merge sub arrays [l...m] [m+1...r]
+ * @array: array to be merged
+ * @l: left
+ * @m: mid
+ * @r: right
+ * Return: Anything
+ */
+void merge(int *array, int l, int m, int r)
+{
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
+	int L[n1], R[n2];
+
+	for (i = 0; i < n1; i++)
+		L[i] = array[l + i];
+	for (j = 0; j < n2; j++)
+		R[j] = array[m + 1 + j];
+
+	i = 0;
+	j = 0;
+	k = l;
+	while (i < n1 && j < n2)
+	{
+		if (L[i] <= R[j])
+		{
+			array[k] = L[i];
+			i++;
+		}
+		else
+		{
+			array[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+	while (i < n1)
+	{
+		array[k] = L[i];
+		i++;
+		k++;
+	}
+	while (j < n2)
+	{
+		array[k] = R[j];
+		j++;
+		k++;
+	}
+}
+/**
+ * sort - create sub arrays
+ * @array: Array to be sorted
+ * @l: left
+ * @r: right
+ * Return: Anything
+ */
+void sort(int *array, int l, int r)
+{
+	int m = l + (r - l) / 2;
+
+	if (l < r)
+	{
+		sort(array, l, m);
+		sort(array, m + 1, r);
+		merge(array, l, m, r);
+	}
+}
+/**
+ * merge_sort - merge sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ * Return: Anything
+ */
+void merge_sort(int *array, size_t size)
+{
+	sort(array, 0, size - 1);
+}
