325306831f1efeb8ba97a4ba3584efbab02188c6
Viiic98 <xathovic@gmail.com>
2021-04-30 20:51:00 -0500

    Fix pep8

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -3,27 +3,27 @@
 
 
 def isWinner(x, nums):
-	""" Prime Game """
-	if not nums or x < 1:
-		return None
-	n = max(nums)
-	game = [True for _ in range(max(n + 1, 2))]
-	for i in range(2, int(pow(n, 0.5)) + 1):
-		if not game[i]:
-			continue
-		for j in range(i * i, n + 1, i):
-			game[j] = False
-	game[0] = game[1] = False
-	c = 0
-	for i in range(len(game)):
-		if game[i]:
-			c += 1
-		game[i] = c
-	pl = 0
-	for n in nums:
-		pl += game[n] % 2 == 1
-	if pl * 2 == len(nums):
-		return None
-	if pl * 2 > len(nums):
-		return "Maria"
-	return "Ben"
+    """ Prime Game """
+    if not nums or x < 1:
+        return None
+    n = max(nums)
+    game = [True for _ in range(max(n + 1, 2))]
+    for i in range(2, int(pow(n, 0.5)) + 1):
+        if not game[i]:
+            continue
+        for j in range(i * i, n + 1, i):
+            game[j] = False
+    game[0] = game[1] = False
+    c = 0
+    for i in range(len(game)):
+        if game[i]:
+            c += 1
+        game[i] = c
+    pl = 0
+    for n in nums:
+        pl += game[n] % 2 == 1
+    if pl * 2 == len(nums):
+        return None
+    if pl * 2 > len(nums):
+        return "Maria"
+    return "Ben"
